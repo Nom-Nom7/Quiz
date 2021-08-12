6c6c5d1e2c622bb581bdef82e58ddb8a69863ad1
Nom-Nom7 <76200202+Nom-Nom7@users.noreply.github.com>
2021-08-12 11:12:29 +0530

    Add files via upload

--- js/Quiz.js
+++ js/Quiz.js
@@ -29,43 +29,32 @@ class Quiz {
   }
 
   play(){
-    //write code here to hide question elements
-    this.question.hide();
-
-    //write code to change the background color here
-    background = "yellow";
-
-    //write code to show a heading for showing the result of Quiz
-    var title = createElement('h2');
-    title.html("Here are the Results!");
-    title.position(400,100);
-
-    //call getContestantInfo( ) here
-    contestant.getPlayerInfo();
-
-    //write condition to check if contestantInfor is not undefined
+    question.hide();
+    background("Yellow");
+    fill(0);
+    textSize(30);
+    text("Result of the Quiz",340, 50);
+    text("----------------------------",320, 65);
+    Contestant.getPlayerInfo();
     if(allContestants !== undefined){
+      debugger;
+      var display_Answers = 230;
       fill("Blue");
       textSize(20);
-      textSize("NOTE: Contestants who answered correct are highlited in green color!",130,230);
+      text("*NOTE: Contestant who answered correct are highlighted in green color!",130,230);
 
       for(var plr in allContestants){
+        debugger;
         var correctAns = "2";
-       if(correctAns === allContestants[plr].answer){
-         fill("green");
-       }
+        if (correctAns === allContestants[plr].answer)
+          fill("Green")
+        else
+          fill("red");
 
-         else{
-           fill("red");
-         }
-       }
+        display_Answers+=30;
+        textSize(20);
+        text(allContestants[plr].name + ": " + allContestants[plr].answer, 250,display_Answers)
       }
-   }
-
-    //write code to add a note here
-
-    //write code to highlight contest who answered correctly
-    
+    }
   }
-
-
+}
